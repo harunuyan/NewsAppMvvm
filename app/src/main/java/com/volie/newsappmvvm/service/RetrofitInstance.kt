d2e7c5193436ac0926b66161f8c99c37a1d2daf1
harun <harunuyan6@gmail.com>
2023-03-06 21:47:46 +0300

    edited retrofit instance

--- app/src/main/java/com/volie/newsappmvvm/service/RetrofitInstance.kt
+++ app/src/main/java/com/volie/newsappmvvm/service/RetrofitInstance.kt
@@ -7,21 +7,20 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
-    companion object {
-        private val retrofit by lazy {
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-            val client = OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build()
-            Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-        }
-        val api by lazy {
-            retrofit.create(NewsAPI::class.java)
-        }
+    private val retrofit by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    val api by lazy {
+        retrofit.create(NewsAPI::class.java)
     }
 }
