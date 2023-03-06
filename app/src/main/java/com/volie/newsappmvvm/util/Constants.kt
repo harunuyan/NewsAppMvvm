eb72bded100b32db343d5a05ebde3185784d5a02
harun <harunuyan6@gmail.com>
2023-03-06 15:10:12 +0300

    implemented api

---
+++ app/src/main/java/com/volie/newsappmvvm/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.volie.newsappmvvm.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "38572060992e4dd0bc200b6fb0ce0f9c"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
