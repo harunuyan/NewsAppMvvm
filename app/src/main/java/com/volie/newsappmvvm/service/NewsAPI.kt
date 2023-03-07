738269b1892280170cf8db3fa11ba787b8828893
harun <harunuyan6@gmail.com>
2023-03-07 21:39:34 +0300

    edited API extention URL

--- app/src/main/java/com/volie/newsappmvvm/service/NewsAPI.kt
+++ app/src/main/java/com/volie/newsappmvvm/service/NewsAPI.kt
@@ -8,14 +8,14 @@ import retrofit2.http.Query
 
 interface NewsAPI {
 
-    @GET("v2/top-headlines")
+    @GET("v2/top-headlines?country=us&apiKey=38572060992e4dd0bc200b6fb0ce0f9c")
     suspend fun getBreakingNews(
-        @Query("country")
+/*        @Query("country")
         countryCode: String = "us",
         @Query("page")
         pageNumber: Int = 1,
         @Query("apiKey")
-        apiKey: String = API_KEY
+        apiKey: String = API_KEY*/
     ): Response<NewsResponse>
 
     @GET("v2/everything")
